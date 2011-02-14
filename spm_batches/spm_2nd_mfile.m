84c77023056ce4db38fe25f0e8072f50ac48a231
Scott Burns <sburns@nmr.mgh.harvard.edu>
2011-02-14 14:03:14 -0500

    Removed all spm_batches/ files

--- spm_batches/spm_2nd_mfile.m
+++
@@ -1,9 +0,0 @@
-warning off all;
-try
-	spm('defaults','fmri');
-	spm_jobman('initcfg');
-	output = spm_jobman('run_nogui','[script]');
-catch ME
-	sendmail('sburns@nmr.mgh.harvard.edu','[study] 2nd level stats failed.');
-end
-exit;
