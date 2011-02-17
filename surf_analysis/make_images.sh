6901db75003cd180b8f15adf22f1d4a0bf320fe8
Scott Burns <sburns@nmr.mgh.harvard.edu>
2011-02-17 11:32:48 -0500

    Made surf_analysis folder
    In this folder, there are scripts for generating images (used by the
    pipeline in --surf_second) and html files used in --package_second.

---
+++ surf_analysis/make_images.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir img
+tksurfer fsaverage rh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl rh_tiff
+tksurfer fsaverage lh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl lh_tiff
+cd img
+for side in RH LH ; do 
+	for view in Anterior Posterior Medial Lateral Superior Inferior ; do
+		convert $side"_"$view.tiff $side"_"$view.jpg
+		rm $side"_"$view.tiff
+	done
+done
+cd ..
+
