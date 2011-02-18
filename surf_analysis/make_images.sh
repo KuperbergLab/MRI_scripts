07039604584c3fca4ebabb02b052d1e0183ba559
Scott Burns <sburns@nmr.mgh.harvard.edu>
2011-02-18 14:23:08 -0500

    Improvements to --surf_second
    Added --dry: sets up surf_scripts but doesn't invoke them.
    Added --aparc: tells tksurfer to load automatic parcellation files.
    Major change to how options are stored. No longer need to manually
    put options into the data structure.  The for loop beginning at ln857
    does this automatically.

--- surf_analysis/make_images.sh
+++ surf_analysis/make_images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir img
-tksurfer fsaverage rh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl rh_tiff
-tksurfer fsaverage lh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl lh_tiff
+tksurfer fsaverage rh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl rh_tiff	[aparc]
+tksurfer fsaverage lh inflated -overlay spmT_0001.img -mni152reg -fthresh [tvalue] -tcl lh_tiff [aparc]
 cd img
 for side in RH LH ; do 
 	for view in Anterior Posterior Medial Lateral Superior Inferior ; do
