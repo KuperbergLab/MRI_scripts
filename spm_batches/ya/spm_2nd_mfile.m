757586ac6d873e454e1f172c3af1394d8fcf445f
Scott Burns <sburns@nmr.mgh.harvard.edu>
2011-02-24 09:57:31 -0500

    Improved Second level setup
    Before, there was a single monolithic bactch for each study. While simple,
    it was cumbersome to add new contrasts.  Now, each contrast gets its own
    <contrast>_job.m (the batch) and <contrast>.m (the script) in
    <study>/<date>/jobs/.

--- spm_batches/ya/spm_2nd_mfile.m
+++ spm_batches/ya/spm_2nd_mfile.m
@@ -3,7 +3,8 @@ try
 	spm('defaults','fmri');
 	spm_jobman('initcfg');
 	output = spm_jobman('run_nogui','[batch]');
+	sendmail('sburns@nmr.mgh.harvard.edu','[study] [contrast] 2nd level stats succeeded.');
 catch ME
-	sendmail('sburns@nmr.mgh.harvard.edu','[study] 2nd level stats failed.');
+	sendmail('sburns@nmr.mgh.harvard.edu','[study] [contrast] 2nd level stats failed.');
 end
 exit;
