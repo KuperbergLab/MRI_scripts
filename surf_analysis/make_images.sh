950bb327e684a2b67375a434461b30d75b566aa1
Scott Burns <sburns@nmr.mgh.harvard.edu>
2011-03-09 09:50:42 -0500

    Added arrow pngs and new image processing
    make_images.sh now blends the TkSurfer output image with an arrow
    file that contains RAS arrows to help the viewer orient the image
    in space.

--- surf_analysis/make_images.sh
+++ surf_analysis/make_images.sh
@@ -4,30 +4,30 @@ mkdir img
 tksurfer fsaverage rh inflated -overlay spmT_0001.img -mni152reg -fthresh $tvalue -tcl rh_tiff	$aparc
 tksurfer fsaverage lh inflated -overlay spmT_0001.img -mni152reg -fthresh $tvalue -tcl lh_tiff $aparc 
 
-convert img/RH_Anterior.tiff img/RH_Anterior.jpg
+compsite -blend 100x100 img/RH_Anterior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Anterior_arrow.jpg img/RH_Anterior.jpg
 rm img/RH_Anterior.tiff
-convert img/RH_Posterior.tiff img/RH_Posterior.jpg
+composite -blend 100x100 img/RH_Posterior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Posterior_arrow.jpg img/RH_Posterior.jpg
 rm img/RH_Posterior.tiff
-convert img/RH_Medial.tiff img/RH_Medial.jpg
+composite -blend 100x100 img/RH_Medial.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Medial_arrow.jpg img/RH_Medial.jpg
 rm img/RH_Medial.tiff
-convert img/RH_Lateral.tiff img/RH_Lateral.jpg
+composite -blend 100x100 img/RH_Lateral.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Lateral_arrow.jpg img/RH_Lateral.jpg
 rm img/RH_Lateral.tiff
-convert img/RH_Superior.tiff img/RH_Superior.jpg
+composite -blend 100x100 img/RH_Superior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Superior_arrow.jpg img/RH_Superior.jpg
 rm img/RH_Superior.tiff
-convert img/RH_Inferior.tiff img/RH_Inferior.jpg
+composite -blend 100x100 img/RH_Inferior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/RH_Inferior_arrow.jpg img/RH_Inferior.jpg
 rm img/RH_Inferior.tiff
 
-convert img/LH_Anterior.tiff img/LH_Anterior.jpg
+composite -blend 100x100 img/LH_Anterior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Anterior_arrow.jpg img/LH_Anterior.jpg
 rm img/LH_Anterior.tiff
-convert img/LH_Posterior.tiff img/LH_Posterior.jpg
+composite -blend 100x100 img/LH_Posterior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Posterior_arrow.jpg img/LH_Posterior.jpg
 rm img/LH_Posterior.tiff
-convert img/LH_Medial.tiff img/LH_Medial.jpg
+composite -blend 100x100 img/LH_Medial.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Medial_arrow.jpg img/LH_Medial.jpg
 rm img/LH_Medial.tiff
-convert img/LH_Lateral.tiff img/LH_Lateral.jpg
+composite -blend 100x100 img/LH_Lateral.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Lateral_arrow.jpg img/LH_Lateral.jpg
 rm img/LH_Lateral.tiff
-convert img/LH_Superior.tiff img/LH_Superior.jpg
+composite -blend 100x100 img/LH_Superior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Superior_arrow.jpg img/LH_Superior.jpg
 rm img/LH_Superior.tiff
-convert img/LH_Inferior.tiff img/LH_Inferior.jpg
+composite -blend 100x100 img/LH_Inferior.tiff /cluster/kuperberg/SemPrMM/MRI/scripts/surf_analysis/LH_Inferior_arrow.jpg img/LH_Inferior.jpg
 rm img/LH_Inferior.tiff
 
 
