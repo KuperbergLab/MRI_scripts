60ca79b24392fd78f6e1aed8a36f69723da5355c
Ellen Lau <ellenlau@umd.edu>
2012-01-27 13:48:41 -0500

    recreating roi shell scripts

--- roi_scripts/sess.sh
+++ roi_scripts/sess.sh
@@ -1,45 +1,23 @@
-funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-074.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-034.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-037.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.lh.BA45.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-044.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-025.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.lh.BA44.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-073.roicfg  & 
+funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.S_temporal_sup.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_temp_sup-Lateral.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_temporal_inf.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.Pole_temporal.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_pariet_inf-Angular.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.S_temporal_inf.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_front_inf-Triangul.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_front_inf-Orbital.roicfg  & 
  wait
-funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-012.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.fir.aparc2009-lh-038.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-074.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-034.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-037.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.lh.BA45.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-044.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-025.roicfg  & 
+funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_front_inf-Opercular.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.lh.shrf.G_temporal_middle.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.S_temporal_sup.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_temp_sup-Lateral.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_temporal_inf.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.Pole_temporal.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_pariet_inf-Angular.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.S_temporal_inf.roicfg  & 
  wait
-funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.lh.BA44.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-073.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-012.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.lh.spm.aparc2009-lh-038.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-074.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-034.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-037.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.rh.BA45.roicfg  & 
- wait
-funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-044.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-025.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.rh.BA44.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-073.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-012.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.fir.aparc2009-rh-038.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-074.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-034.roicfg  & 
- wait
-funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-037.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.rh.BA45.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-044.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-025.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.rh.BA44.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-073.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-012.roicfg  & 
- funcroi-sess -sf ya.ATLLoc.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/functionals/roi_cfg/ATLLoc.rh.spm.aparc2009-rh-038.roicfg  & 
- wait
+funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_front_inf-Triangul.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_front_inf-Orbital.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_front_inf-Opercular.roicfg  & 
+ funcroi-sess -sf sc.group-analysis/sc.MaskedMM.sessid -d /cluster/kuperberg/SemPrMM/MRI/functionals/ -roi /cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_cfg/MaskedMM.rh.shrf.G_temporal_middle.roicfg  & 
+ 
