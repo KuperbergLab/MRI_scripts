4e16426ddfb83f254f646502d9e9382305c06e6c
Ellen Lau <ellenlau@umd.edu>
2012-02-01 10:24:15 -0500

    modified roi_barplot scripts to include error bars

--- roi_scripts/roi_barplot.R
+++ roi_scripts/roi_barplot.R
@@ -1,22 +1,33 @@
+error.bar <- function(x, y, upper, lower=upper, length=0.1,...)
+	{
+		if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+		stop("vectors must be same length")
+		arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+	}
+
+
 roi_barplot <-function(subj_gp, exp, label){
 
 	dataPath = '/cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/R/'
-	
 	load(paste(dataPath,subj_gp,'_',exp,'_roiTable.df',sep=''))
+
 	##subsetting
 	roiData.label = subset(roiData.all, roi==label, select=c(subj, cond, hemCode, signal))
 	attach(roiData.label)
-	roiData.label.table = tapply(signal, list(cond, hemCode), mean)
+	roiData.label.mean = tapply(signal, list(cond, hemCode), mean)
+	roiData.label.sd = tapply(signal, list(cond, hemCode), sd)
+	roiData.label.n = tapply(signal, list(cond,hemCode),length)
+	roiData.label.stderr = roiData.label.sd/sqrt(roiData.label.n)  #for error bars	
 	detach(roiData.label)
 	
 	##plotting
 	##for documentation see http://www.stat.berkeley.edu/classes/s133/saving.html
 	pdf(paste(dataPath, 'barplots/',subj_gp,'_', exp,'_', label, '.pdf', sep=''))
-	barplot(roiData.label.table, beside=TRUE, main= label)
+	bp <- barplot(roiData.label.mean, beside=TRUE, main= label)
+	error.bar(bp, roiData.label.mean, roiData.label.stderr)
+	
+	
 	dev.off()
-	
-	
-	
 	#aggLabel = aggregate(signal, by=list(cond, roi, hemCode), FUN=mean, na.rm=TRUE)
 
 }
--- roi_scripts/roi_barplot_hemSummary.R
+++ roi_scripts/roi_barplot_hemSummary.R
@@ -1,3 +1,12 @@
+error.bar <- function(x, y, upper, lower=upper, length=0.1,...)
+{
+	if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+	stop("vectors must be same length")
+	arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+}
+
+
+
 roi_barplot_hemSummary <-function(subj_gp, exp,hem){
 
 	dataPath = '/cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/R/'
@@ -6,17 +15,24 @@ roi_barplot_hemSummary <-function(subj_gp, exp,hem){
 	##subsetting
 	roiData.hem = subset(roiData.all, hemCode==hem, select=c(subj, cond, roi, signal))
 	attach(roiData.hem)
-	roiData.hem.table = tapply(signal, list(cond, roi), mean)
+	roiData.hem.mean = tapply(signal, list(cond, roi), mean)
+	roiData.hem.sd = tapply(signal, list(cond, roi), sd)
+	roiData.hem.n = tapply(signal, list(cond,roi),length)
+	roiData.hem.stderr = roiData.hem.sd/sqrt(roiData.hem.n)  #for error bars	
 	detach(roiData.hem)
 	
-	rownames(roiData.hem.table)
+	rownames(roiData.hem.mean)
 	
 	labelx = cbind('BA44','BA47','BA45','AG','ITG','MTG','STG','TP','ITS','STS')
 	
 	##plotting
 	##for documentation see http://www.stat.berkeley.edu/classes/s133/saving.html
 	pdf(paste(dataPath, 'barplots/',subj_gp,'_', exp,'_', hem, '.pdf', sep=''))
-	barplot(roiData.hem.table, beside=TRUE, main= hem, legend=rownames(roiData.hem.table), names.arg=labelx, cex.names=0.8, ylim=c(-.1, .1))
+	bp <-barplot(roiData.hem.mean, beside=TRUE, main= paste(hem,'n=',roiData.hem.n[1]), legend=rownames(roiData.hem.mean), names.arg=labelx, cex.names=0.8, ylim=c(-.15, .15))
+	error.bar(bp, roiData.hem.mean, roiData.hem.stderr)
+
+	
+	
 	dev.off()
 	
 	
