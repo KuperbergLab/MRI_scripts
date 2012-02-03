74947728011c39f785f1dca1d763c78610d1fba4
Ellen Lau <ellenlau@umd.edu>
2012-02-02 20:07:35 -0500

    fixed roistats2R to handle any number of contrasts, changed variable names for readability

--- roi_fsfast/roistats2R.py
+++ roi_fsfast/roistats2R.py
@@ -3,10 +3,15 @@ import os
 import writeOutput
 import readInput
 
+##This script takes as input the roi fsfast table output by make_func_roi_results.py, and outputs
+##the same data in long format appropriate for R
+
+##Usage ex: python roistats2R.py ya ATLLoc
+
 def roistats2R(subj_gp, exp):
 
-        trialCount=rtrialCount = r= 0
-        dataTable1=lineTemp=line=dataLine=Con=[]
+        tableLength=0
+        dataTable=lineTemp=line=Con=dataLine=[]
         allData=[]
         Contrasts = []
         subj = []
@@ -14,53 +19,51 @@ def roistats2R(subj_gp, exp):
         roiName = []
         
         inFile = "/cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/roi_summary/" + subj_gp +"_" + exp +"FullTable.txt"
-        dataTable1 = readInput.readTable(inFile)
-        trialCount = len(dataTable1)     
+        dataTable = readInput.readTable(inFile)
+        tableLength = len(dataTable)     
         
-        for i in range(1, trialCount):
-            lineTemp = dataTable1[i]
-            Con = dataTable1[0]
+        ##parse the data to figure out number of contrasts, rois, and subjects
+        for i in range(1, tableLength):   #skip the header row
+            lineTemp = dataTable[i]
+            Con = dataTable[0]
             if len(lineTemp)<6:
                    roiName.append(str(lineTemp[2]))
             elif len(lineTemp)>4:
                    subj.append(lineTemp[0])
                    
         subj_num = (len(subj)/len(roiName))
-        [Con[1], x] = str.split(str(Con[1]), '_')
-        [Con[3], x] = str.split(str(Con[3]), '_')
-        [Con[5], x] = str.split(str(Con[5]), '_')
-        Contrasts.append(Con[1])
-        Contrasts.append(Con[3])
-        Contrasts.append(Con[5])
-##        print Contrasts
-##        for i in range(0, subj_num):
-##                print subj[i]
         
+		##Get out the contrasts
+        numContrasts = (len(Con)-1)/2   ##figure out how many contrasts there are
+        for c in range(numContrasts):
+        	conIndex = c*2+1
+        	[Con[conIndex],x] = str.split(str(Con[conIndex]), '_')
+        	Contrasts.append(Con[conIndex])
+##        print Contrasts
+
         i = 0
-        for i in range(1, trialCount):
-             line = dataTable1[i]
-             if len(line) < 4:
-                          c_label = line[2]
-##                          print c_label
+        for i in range(1, tableLength):  #skip the header row
+             line = dataTable[i]
+             curr_subj = line[0]
+             if len(line) < 4:  ##This is the beginning of a new label block so update current label
+                          curr_label = line[2]
+##                          print curr_label
              if len(line) > 4:
-                          count=1  ##for signal values
-                          for l in range(0, len(Contrasts)):
+                          for c in range(numContrasts):
                                         for m in [0, 1]:
-                                                if count<7:
-                                                        c_contrast = str(Contrasts[l])
-                                                        c_hem = hem[m]
-                                                        c_signal = line[count]
-                                                        c_subj = line[0] 
-                                                        dataLine = [c_subj, c_contrast, c_label, c_hem, c_signal]
-                                                        allData.append(dataLine)
-                                                        dataLine = []
-                                                        count =count+1
+													curr_contrast = str(Contrasts[c])
+													curr_hem = hem[m]
+													curr_signal = line[c*2+1+m]  #for each contrast, this will find correct signal
+##													print c*2+1+m
+													dataLine = [curr_subj, curr_contrast, curr_label, curr_hem, curr_signal]
+													allData.append(dataLine)
+													dataLine = []
                                                         
 
         outFile = "/cluster/kuperberg/SemPrMM/MRI/results/roi_fsfast/R/" + subj_gp +"_" + exp + "_" + "roiTable.txt"                                              
         writeOutput.writeTable(outFile, allData)                                            
 
-##        print allData
+
         
 if __name__ == "__main__":
         
