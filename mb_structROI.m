6f3489d837eaff0bd37e0ca0915f0be23ac0bdcc
Ellen Lau <ellenlau@umd.edu>
2012-02-23 15:47:02 -0500

    changed input directory for mb_structROI.m

--- mb_structROI.m
+++ mb_structROI.m
@@ -1,14 +1,14 @@
 function [conArray,p,stats] = mb_structROI(exp, listPrefix, contrastName,roi_file)
 
-%ex: [conArray, p, stats] = mb_structROI('BaleenHP','Unrelated Target vs. Related Target - All Sessions', 'MNI_Temporal_Pole_Sup_L_roi.mat')
+%ex: [conArray, p, stats] = mb_structROI('BaleenHP','ya_mri_baleenhp','Unrelated Target vs. Related Target - All Sessions', 'MNI_Temporal_Pole_Sup_L_roi.mat')
 
 %ex: [conArray, p, stats] = mb_structROI('ATLLoc','Sentences vs. WordList - All Sessions', 'MNI_Temporal_Mid_L_roi.mat')
 
 %ex: [conArray, p, stats] = mb_structROI('ATLLoc','Unrelated vs. Direct - All Sessions', 'MNI_Temporal_Mid_L_roi.mat')
 
 
-dataPath = '/autofs/cluster/kuperberg/SemPrMM/MEG/';
-subjList = (dlmread(strcat(dataPath,'scripts/function_inputs/',listPrefix, '.txt')))';
+dataPath = '/autofs/cluster/kuperberg/SemPrMM/MRI/';
+subjList = (dlmread(strcat(dataPath,'scripts/input/',listPrefix,'_mb.txt')))';
 
 %subjList = [1 2 3 4 5 6 7 9 12 15 16 17 18 19 20 21 22 23 24 25 26 27 29 30 31 32 33]
 spm('defaults','fmri');
@@ -22,7 +22,7 @@ roi_dir = '/autofs/cluster/kuperberg/Software/spm/toolbox/marsbar-aal-0.2';
 
 for s = subjList
     s
-    root_dir = strcat('/autofs/cluster/kuperberg/SemPrMM/MRI/functionals/ya',int2str(s),'/',exp,'/stats_outliers/8mm_fs_pp/');
+    root_dir = strcat('/autofs/cluster/kuperberg/SemPrMM/MRI/functionals/ya',int2str(s),'/',exp,'/stats_outliers/8mm/');
     conf_design_name = 'SPM.mat';
     
     roi_name = fullfile(roi_dir, roi_file);
