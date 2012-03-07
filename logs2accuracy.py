be59b5e51cf38428ee6d699e64430e5b3942bf17
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-03-07 12:26:48 -0500

    Runs for all 3 paradigms for specified subjectType. Saves 2 results - short form in MRI/results/behavioral_accuracy folder, and long form in MRI/results/behavioral_accuracy/R folder to be used by R.

--- logs2accuracy.py
+++ logs2accuracy.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-import os
+import sys
+import os 
+import os.path
+import readInput
+from operator import itemgetter, attrgetter
 from glob import glob 
 
 modalities = ['MRI']
 studies = ['MaskedMM','BaleenMM','AXCPT']
+subjType = ['ac', 'sc', 'ya']
 studies_tasks = dict({'MaskedMM':[4, 5],'BaleenMM':[5, 10, 11, 12],'AXCPT':[4]})
 studies_notasks = dict({'MaskedMM':[1, 2, 3], 'BaleenMM':[1, 2, 4, 6, 7, 8, 9],'AXCPT':[1,2,3] })
 study_code = dict({'BaleenMM':9,'MaskedMM':9,'AXCPT':9})
@@ -23,10 +28,14 @@ def parse_logfile(path):
 	all_split = [line.split() for line in all_lines]
 	#all_split[i][16] is code, all_split[i][19] is rt
 	study = all_split[0][0]
-	if study is 'AX-CPT':
+	#print(study)
+	if study == 'AXCPT_SC' or study=='AX-CPT': #vtsd logs for ac and sc have _SC tagged to their name. 
 		study = 'AXCPT'
+	elif study =='MaskedMM_SC':
+                study = 'MaskedMM'
 	if study not in studies:
 		return [study, None]
+	#print(study)
 	results = dict()
 	all_codes = set([line[study_code[study]] for line in all_split])
 	for code in all_codes:
@@ -61,18 +70,69 @@ def parse_study(modality,study,subjects):
 		all_subs[sub] = subj_results
 	return all_subs
 	
-def get_subjects(modality,study):
+def get_subjects(modality,study,subjType):
 	path = os.path.join('/cluster/kuperberg/SemPrMM/',modality,'vtsd_logs')+os.sep
 	all_dirs = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
-	subs =  [subj for subj in all_dirs if subj.startswith('y')]
+	#print(all_dirs)
+	if subjType == 'ac':
+                subs =  [subj for subj in all_dirs if subj.startswith('a')]
+        elif subjType == 'sc':
+                subs =  [subj for subj in all_dirs if subj.startswith('s')]
+        else:
+                subs =  [subj for subj in all_dirs if subj.startswith('y')]
+                
 	return [sub for sub in subs if len(glob(os.path.join(path,sub,study+'*log'))) > 0]
+        print(subs)
 
-def main():
-	for mod in modalities:
+def mriaccuracy2R(modality,study,subjType):
+        data_path = '/cluster/kuperberg/SemPrMM/MRI/'
+        inFile1 = data_path + 'results/behavioral_accuracy/' + modality + '_'+ subjType + '_' + str(study)+ "_accuracy-short.log"
+        outFile1 = data_path + 'results/behavioral_accuracy/R/' + modality + '_'+ subjType +'_' + str(study)+ "_accuracy.log"    
+        totalTrials = 0 # Must be initialised within the loop to refresh for each subject :) 
+        dataTable1=[]
+        dataTable2=[]
+        lineTemp = []
+        lineTemp1 = []
+        temp = 1
+        header = []
+        if os.path.exists(inFile1):
+            print("File exists")
+        else:
+            print("Check the input paradigm file name")
+        myFile1 = open(inFile1, "r") 
+        while temp:
+             temp = myFile1.readline()
+             temp1 = temp.strip() # not ('')
+             if temp1:
+                  temp2 = temp1.split()
+                  dataTable1.append(temp2) # Save information as a list of items in a DataTable
+                  runCount=len(dataTable1)   
+        myFile1.close()
+
+        header = dataTable1[0]
+        cond_len = len(header)
+
+        myFile2 = open(outFile1, "w")
+        myFile2.write("Sub:\t\tCond\t\tAccuracy\n")	
+        for i in range(1, runCount):
+               lineTemp = (dataTable1[i])
+               #print(lineTemp[0])
+               if (lineTemp[0] == 'ya13:' and study == 'BaleenMM' or lineTemp[0] == 'ac4:'): #removing ya13 BaleenMM and ac4 from the accuracy calculation due to incomplete data.
+                       continue
+               for j in range(1,cond_len):
+                             myFile2.write(lineTemp[0] + '\t\t')
+                             myFile2.write(header[j] + '\t\t')
+                             myFile2.write(lineTemp[j])
+                             myFile2.write('\n')
+        
+
+def main(subjType):
+        for mod in modalities:
 		for study in studies:
-			subjects = get_subjects(mod,study)
+			subjects = get_subjects(mod,study,subjType)
+			print(study)
 			study_results = parse_study(mod,study,subjects)
-			with open('/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy'+mod+'_'+study+'_accuracy.log','w') as f:
+			with open('/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy/'+mod+'_'+subjType+'_'+study+'_accuracy-short.log','w') as f:
 				#write out header
 				f.write('sub:\t\t')
 				for code in code_names[study]:
@@ -82,7 +142,7 @@ def main():
 					num = den = 0
 					f.write(sub+':\t\t')
 					results = study_results[sub]
-					print(study,sub,results)
+					#print(study,sub,results)
 					#sort codes numerically
 					codes = sorted([int(key) for key in results.keys()])
 					num = sum([item[0] for code,item in results.items() if int(code) in studies_tasks[study]])
@@ -91,6 +151,12 @@ def main():
 					for result in sorted_results:
 						f.write(str(round(result,3))+'\t\t')
 					f.write(str(round(float(num)/den,3))+'\n')
+                        mriaccuracy2R(mod,study,subjType)
+        print("Completed. See results in /MRI/results/behavioral_accuracy/R/ folder")
+
+
+
+
 						
 if __name__ == "__main__":
-	main()
+	main(sys.argv[1])
