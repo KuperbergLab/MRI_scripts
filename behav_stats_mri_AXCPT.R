b59a3ddfbfb4f3766fd2f07da778e18fd35d5b22
Ellen Lau <ellenlau@umd.edu>
2012-04-10 18:12:24 -0400

    added plotting to behav_stats_mri_AXCPT

--- behav_stats_mri_AXCPT.R
+++ behav_stats_mri_AXCPT.R
@@ -22,7 +22,33 @@ numSubj <-nlevels(factor(behavData.all$subj, exclude= NA))
 print(paste("Table of Mean Accuracies, n:", numSubj,sep=" "))
 print(model.tables(behavData.all.aov, "means"), digits = 5)
 
+###########
+###########PLOTS
 
+error.bar <- function(x, y, upper, lower=upper, length=0.1,...)
+	{
+		if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+		stop("vectors must be same length")
+		arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+	}
+
+
+
+behavData.all.mean <- tapply(behavData.all$acc, behavData.all$cond, mean)
+behavData.all.sd = tapply(behavData.all$acc, behavData.all$cond, sd)
+behavData.all.n = tapply(behavData.all$acc, behavData.all$cond,length)
+print("n")
+print(behavData.all.n)
+behavData.all.stderr = behavData.all.sd/sqrt(behavData.all.n)  #for error bars	
+
+	##plotting
+	##for documentation see http://www.stat.berkeley.edu/classes/s133/saving.html
+	#pdf(paste(filePath, 'barplots/',subjType,'_AXCPT_acc.pdf', sep=''))
+	bp <- barplot(behavData.all.mean, beside=TRUE, main= paste("AXCPT"),ylim=c(0, 1))
+	error.bar(bp, behavData.all.mean, behavData.all.stderr)
+	
+	
+	#dev.off()
 
 ##################################################
 ##################ANOVAS##########################
@@ -40,6 +66,7 @@ behavData.ayby = subset(behavData.all, cond == 'AY' | cond == 'BY')
 eztest <-ezANOVA(data=behavData.ayby,dv = .(acc),wid=.(subj),within=.(cond),type=3,detailed=TRUE)
 print("Paired comparison AY vs BY")
 print(eztest)
+
 
 sink()
 
