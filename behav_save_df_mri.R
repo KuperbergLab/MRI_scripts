27d19d34af9702482c0bafd3853b5ab0b6cd8305
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-04-10 17:23:44 -0400

    behav_save_df_mri.R: modified to read the listPrefix and exp name as inputs

--- behav_save_df_mri.R
+++ behav_save_df_mri.R
@@ -1,11 +1,11 @@
-behav_save_df_mri <-function(subjType,exp){
+behav_save_df_mri <-function(listPrefix,exp){
 	
 filePath <- "/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy/R/"
-fileName <- paste(filePath,'MRI_',subjType,'_',exp,"_accuracy.log",sep="")
+fileName <- paste(filePath,'MRI_',listPrefix,'_',exp,"_accuracy.log",sep="")
 buffer<-read.table(skip=1, fileName, header=FALSE)
 behavData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),acc=buffer$V3)
 
-outFile <-paste(filePath, 'MRI_', subjType, '_', exp, '_accuracy.df', sep="")
+outFile <-paste(filePath, 'MRI_', listPrefix, '_', exp, '_accuracy.df', sep="")
 save(behavData.all, file=outFile)
 
 }
