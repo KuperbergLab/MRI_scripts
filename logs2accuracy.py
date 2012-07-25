3bb033c788ba018a93b3ed6ea067fce0ea4c6f90
Candida Ustine <candida@nmr.mgh.harvard.edu>
2012-07-25 16:58:35 -0400

    logs2accuracy.py:AXCPT-Included average Correct_Ax Reaction times and average InCorrect BX, BY, AY reaction times in the results

--- logs2accuracy.py
+++ logs2accuracy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+##usage: python logs2accuracy.py ac ac_mri_axcpt AXCPT
 
 import sys
 import os 
@@ -21,6 +22,18 @@ code_names = dict({'MaskedMM':['DirRel','IndRel','Unrel','InsPr','InsTar'],
 
 def parse_logfile(path):
 	all_lines = []
+        rt_ax = 0
+        rt_ay = 0
+        rt_by = 0
+        rt_bx = 0
+        ax_trials = 0
+        bx_trials = 0
+        by_trials = 0
+        ay_trials =0
+        rt_avg_ay= 0
+        rt_avg_by= 0
+        rt_avg_bx= 0
+        rt_avg_ax =0
 	with open(path,'r') as f:
 		all_lines = f.read()
 	f.close()
@@ -28,22 +41,23 @@ def parse_logfile(path):
 	all_split = [line.split() for line in all_lines]
 	#all_split[i][16] is code, all_split[i][19] is rt
 	study = all_split[0][0]
-	#print(study)
+
 	if study == 'AXCPT_SC' or study=='AX-CPT': #vtsd logs for ac and sc have _SC tagged to their name. 
 		study = 'AXCPT'
 	elif study =='MaskedMM_SC':
                 study = 'MaskedMM'
 	if study not in studies:
 		return [study, None]
-	#print(study)
 	results = dict()
 	all_codes = set([line[study_code[study]] for line in all_split])
+	#print all_codes
 	for code in all_codes:
 		results[code] = [0,0]
 	for i,line in enumerate(all_split):
 		code = line[study_code[study]]
 		rt = float(line[study_rt[study]])
 		results[code] = [results[code][0],results[code][1]+1]
+                
 		if int(code) in studies_notasks[study] and rt < 1:
 				results[code] = [results[code][0]+1,results[code][1]]
 		elif (int(code) in studies_tasks[study] 
@@ -51,25 +65,67 @@ def parse_logfile(path):
 			(i+1 < len(all_split) 
 			and (0 <= float(all_split[i+1][study_rt[study]]) - float(line[5]) <= 1.4) ) ) ):
 				results[code] = [results[code][0]+1,results[code][1]]
-	return results
+                if study == 'AXCPT':
+                   if float(line[12]) != 0:
+                        rx_t = rt - float(line[5]) #RT subtracted from the presented time(secs)
+                        if line[7] != 'A' and line[8] == 'X':
+                                rt_bx += rx_t
+                                bx_trials +=1
+                        elif line[7] != 'A' and line[8] != 'X':
+                                rt_by += rx_t
+                                by_trials +=1
+                        elif line[7] == 'A' and line[8] != 'X':
+                                rt_ay += rx_t
+                                ay_trials +=1
+                        elif line[7] == 'A' and line[8] == 'X':
+                                rt_ax += rx_t
+                                ax_trials +=1 
+        rt_avg_ax = float(rt_ax/(ax_trials))
+        if bx_trials !=0:
+                rt_avg_bx = rt_bx/(bx_trials)
+        if ay_trials !=0:
+                rt_avg_ay = rt_ay/(ay_trials)
+        if by_trials !=0:
+                rt_avg_by = rt_by/(by_trials)
+        return results, rt_avg_ay, rt_avg_by, rt_avg_bx, rt_avg_ax 
 
 
-def parse_study(modality,study,subjects):
+def parse_study(modality,study,subjects,listPrefix):
 	all_subs = dict()
-	for sub in subjects:
+        rt_ax_avg =[]
+        rt_ay_avg=[]
+        rt_bx_avg = []
+        rt_by_avg =[]
+        rt_avg_ay= 0
+        rt_avg_by= 0
+        rt_avg_bx= 0
+        rt_avg_ax =0
+        for i, sub in enumerate(subjects):
+                print sub
+                rt_ax_tot= 0
+                rt_ay_tot= 0
+                rt_bx_tot = 0
+                rt_by_tot =0
 		subj_results = dict()
 		study_logs =  glob(os.path.join('/cluster/kuperberg/SemPrMM/',modality,'vtsd_logs',sub,study+'*log'))
-		##print(study_logs)
 		for log in study_logs:
-			results= parse_logfile(log)
+			results, rt_ay, rt_by, rt_bx, rt_ax  = parse_logfile(log)
 			for key in results.keys():
 				code_results = results[key]
 				if key in subj_results:
 					subj_results[key] = [code_results[0]+subj_results[key][0],code_results[1]+subj_results[key][1]]
 				else:
 					subj_results[key] = code_results
-		all_subs[sub] = subj_results
-	return all_subs
+                        rt_ax_tot+=rt_ax
+                        rt_ay_tot+=rt_ay
+                        rt_bx_tot+=rt_bx
+                        rt_by_tot+=rt_by
+                rt_ax_avg.append(round(rt_ax_tot/len(study_logs),3))
+                rt_ay_avg.append(round(rt_ay_tot/len(study_logs),3))
+                rt_bx_avg.append(round(rt_bx_tot/len(study_logs),3))
+                rt_by_avg.append(round(rt_by_tot/len(study_logs),3))
+                all_subs[sub] = subj_results
+	return all_subs, rt_ax_avg, rt_ay_avg, rt_bx_avg, rt_by_avg
 	
 def get_subjects(modality,study,subjType):
 	path = os.path.join('/cluster/kuperberg/SemPrMM/',modality,'vtsd_logs')+os.sep
@@ -82,7 +138,6 @@ def get_subjects(modality,study,subjType):
         else:
                 subs =  [subj for subj in all_dirs if subj.startswith('y')]
         ##subs = ['ya1' 'ya2'
-                
 	return [sub for sub in subs if len(glob(os.path.join(path,sub,study+'*log'))) > 0]
 
 
@@ -110,56 +165,59 @@ def mriaccuracy2R(modality,study,subjType, listPrefix):
                   dataTable1.append(temp2) # Save information as a list of items in a DataTable
                   runCount=len(dataTable1)   
         myFile1.close()
-
         header = dataTable1[0]
         cond_len = len(header)
-
         myFile2 = open(outFile1, "w")
         myFile2.write("Sub:\t\tCond\t\tAccuracy\n")	
         for i in range(1, runCount):
                lineTemp = (dataTable1[i])
-               #print(lineTemp[0][:-1])
                for j in range(1,cond_len-1):
-                             myFile2.write(lineTemp[0][:-1] + '\t\t')  #the [:-1] reference removes the closing colon from the subject code
-                             myFile2.write(header[j] + '\t\t')
-                             myFile2.write(lineTemp[j])
-                             myFile2.write('\n')
+                        myFile2.write(lineTemp[0][:-1] + '\t\t')  #the [:-1] reference removes the closing colon from the subject code
+                        myFile2.write(header[j] + '\t\t')
+                        myFile2.write(lineTemp[j] + '\n')
+                        myFile2.write('\n')
         
 
 def main(subjType, listPrefix, study):
+        rt_ax_avg= []
+        rt_ay_avg= []
+        rt_bx_avg = []
+        rt_by_avg = []
         for mod in modalities:
 		##for study in studies:
 			##subjects = get_subjects(mod,study,subjType)
                         subjects = readInput.readList('/cluster/kuperberg/SemPrMM/MRI/scripts/input/' +listPrefix)
 			print(subjects)
 			print(study)
-			study_results = parse_study(mod,study,subjects)
-			with open('/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy/'+mod+'_'+listPrefix+'_'+study+'_accuracy-short.log','w') as f:
-                                #print f
-				#write out header
-				f.write('sub:\t')
+			study_results, rt_ax_avg, rt_ay_avg, rt_bx_avg, rt_by_avg = parse_study(mod,study,subjects,listPrefix)
+                        with open('/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy/'+mod+'_'+listPrefix+'_'+study+'_accuracy-short.log','w') as f:
+				f.write('sub:\t')#write out header
 				for code in code_names[study]:
 					f.write(code+'\t')
-				f.write('AllTasks\n')	
-				for sub in sorted(study_results.keys()):
+				if study == 'AXCPT':
+                                        f.write('AllTasks\tAY_RT\tBY_RT\tBX_RT\tAX_RT\n')
+                                else:
+                                        f.write('AllTasks\n')                                      
+				for i, sub in enumerate(subjects):
+                                #for i, sub in enumerate(sorted(study_results.keys(), reverse=True)):
 					num = den = 0
 					f.write(sub+':\t')
 					results = study_results[sub]
-					#print(study,sub,results)
 					#sort codes numerically
 					codes = sorted([int(key) for key in results.keys()])
 					num = sum([item[0] for code,item in results.items() if int(code) in studies_tasks[study]])
 					den = sum([item[1] for code,item in results.items() if int(code) in studies_tasks[study]])
 					sorted_results = [float(results[str(code)][0])/float(results[str(code)][1]) for code in codes]
 					for result in sorted_results:
-						f.write(str(round(result,3))+'\t')
-					f.write(str(round(float(num)/den,3))+'\n')
+				            f.write(str(round(result,3))+'\t')
+					f.write(str(round(float(num)/den,3))+'\t')
+					if study == 'AXCPT':
+                                                f.write(str(rt_ay_avg[i])+'\t')
+                                                f.write(str(rt_by_avg[i])+'\t')
+                                                f.write(str(rt_bx_avg[i])+'\t')
+                                                f.write(str(rt_ax_avg[i])+'\n')
                         mriaccuracy2R(mod,study,subjType, listPrefix)
         print("Completed. See results in /MRI/results/behavioral_accuracy/R/ folder")
-
-
-
-
 						
 if __name__ == "__main__":
 	main(sys.argv[1], sys.argv[2], sys.argv[3])
