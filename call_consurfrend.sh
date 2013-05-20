8237c198c703664394eabea570d36bb7cb3dbba5
Candida Ustine <candida@nmr.mgh.harvard.edu>
2013-05-20 17:06:50 -0400

    call_consurfrend.sh: Updated with more comments and added rgb to jpg conversion

--- call_consurfrend.sh
+++ call_consurfrend.sh
@@ -19,17 +19,23 @@ set thresh = $4
 set exthresh = $5
 set swd = /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra
 
-# python write_surfrend_script.py $conname $connum $swd $thresh $exthresh ## Writes the m file that calls the surfrend_canonical scrript that creates the w files
-# 
-# echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
-# 
-# 
-# cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
-# nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra/$conname'.m'
-# 
-# ##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer
-# 
+ ## Writes the m file that calls the surfrend_canonical script that creates the w files
+ python write_surfrend_script.py $conname $connum $swd $thresh $exthresh 
+ 
+ echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
+ 
+ ## Runs the matlab script that creates the w files
+ cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
+ nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra/$conname'.m'
+ 
+ ##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer 
  cd /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra
  tksurfer $subj lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
+## tksurfer fsaverage lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
 
-convert surfrend_canonical-capture.rgb -colorspace RGB surfrend_canonical-capture.jpg
+
+ ##convert the rgb images to jpeg
+ convert surfrend_canonical-Lat.rgb -colorspace RGB surfrend_canonical-Lat.jpg
+ convert surfrend_canonical-Ven.rgb -colorspace RGB surfrend_canonical-Ven.jpg
+ convert surfrend_canonical-Med.rgb -colorspace RGB surfrend_canonical-Med.jpg
+
