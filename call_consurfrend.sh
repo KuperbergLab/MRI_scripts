585bbaeb9dfcb8a4fbebdfeb77e497c10cd6d779
Candida Ustine <candida@nmr.mgh.harvard.edu>
2013-07-15 07:25:06 -0400

    call_consurfrend.sh: Minor updates and changes

--- call_consurfrend.sh
+++ call_consurfrend.sh
@@ -17,25 +17,28 @@ set subj = $2
 set par = $3
 set thresh = $4
 set exthresh = $5
-set swd = /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra
-
- ## Writes the m file that calls the surfrend_canonical script that creates the w files
- python write_surfrend_script.py $conname $connum $swd $thresh $exthresh 
+set swd = /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra_slice
+# 
+## Writes the m file that calls the surfrend_canonical script that creates the w files
+python /cluster/kuperberg/SemPrMM/MRI/scripts/write_surfrend_script.py $conname $connum $swd $thresh $exthresh 
+  
+echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
  
- echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
- 
- ## Runs the matlab script that creates the w files
- cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
- nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra/$conname'.m'
- 
- ##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer 
- cd /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra
- tksurfer $subj lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
-## tksurfer fsaverage lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
+## Runs the matlab script that creates the w files
+cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
+nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra_slice/$conname'.m'
 
+## Writes the tickle script that specifies the tksurfer Snapshot images and locations
+python /cluster/kuperberg/SemPrMM/MRI/scripts/write_tickle_script.py $conname $swd lh ##Specifiy the hemisphere here, default lh
+ 
+##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer 
+cd /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra_slice
+tksurfer spm_canonical lh inflated -overlay $conname-lh.w -tcl $conname-lh.tcl
+#tksurfer fsaverage lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
 
- ##convert the rgb images to jpeg
- convert surfrend_canonical-Lat.rgb -colorspace RGB surfrend_canonical-Lat.jpg
- convert surfrend_canonical-Ven.rgb -colorspace RGB surfrend_canonical-Ven.jpg
- convert surfrend_canonical-Med.rgb -colorspace RGB surfrend_canonical-Med.jpg
+##convert the rgb images to jpeg
+cd /autofs/cluster/kuperberg/SemPrMM/MRI/functionals/$subj/$par/stats_outliers/swra_slice
+convert $conname-Lat.rgb -colorspace RGB $conname-Lat.jpg
+convert $conname-Ven.rgb -colorspace RGB $conname-Ven.jpg
+convert $conname-Med.rgb -colorspace RGB $conname-Med.jpg
 
