9bdd975590a8acfcf1b5aeee3aed3c5844638b2b
Candida Ustine <candida@nmr.mgh.harvard.edu>
2013-08-02 08:37:36 -0400

    wfile_to_rgb.tcl:Minor changes, removed scale display

--- wfile_to_rgb.tcl
+++ wfile_to_rgb.tcl
@@ -1,11 +1,12 @@
 set colscalebarflag 1
-set file '/autofs/cluster/kuperberg/SemPrMM/MRI/functionals/ac1/ATLLoc/stats_outliers/swra/surfrend_canonical-lh.w'
+set file '/autofs/cluster/kuperberg/SemPrMM/MRI/functionals/ac1/BaleenMM/stats_outliers/swra_slice/RelatedLP-lh.w'
 #foreach file {} {
 set val $file
 #sclv_read_binary_values 0 ##sclv_read_binary_values <field> Where <filename> is the .w file name and <field> is 0-9, signifying which overlay layer you want to load this into.
 sclv_read_from_dotw 0
 redraw
-set outpth "/cluster/kuperberg/SemPrMM/MRI/functionals/ac1/ATLLoc/stats_outliers/swra/"
+set outpth "/cluster/kuperberg/SemPrMM/MRI/functionals/ac1/BaleenMM/stats_outliers/swra_slice"
+set conname "RelatedLP"
 
 rotate_brain_y 90
 sclv_set_current_field 0
@@ -13,18 +14,18 @@ UpdateAndRedraw
 puts "Taking Snapshots..."
 make_lateral_view
 redraw
-set rgb "surfrend_canonical-Lat.rgb"
+set rgb "RelatedLP-Lat.rgb"
 save_rgb
 
 rotate_brain_x 90
 redraw
-set rgb "surfrend_canonical-Ven.rgb"
+set rgb "RelatedLP-Ven.rgb"
 save_rgb
 
 make_lateral_view
 rotate_brain_y 180
 redraw
-set rgb "surfrend_canonical-Med.rgb"
+set rgb "RelatedLP-Med.rgb"
 save_rgb
 
 
