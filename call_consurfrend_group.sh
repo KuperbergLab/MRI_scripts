391afb14035152a9291e6ccc6314ee9b32a2a13c
Candida Ustine <candida@nmr.mgh.harvard.edu>
2013-08-02 08:43:29 -0400

    call_consurfrend_group.sh: Inserted invphaseflag for deactivation pattern, other minor changes

--- call_consurfrend_group.sh
+++ call_consurfrend_group.sh
@@ -17,26 +17,28 @@ set pval = $3
 set exthresh = $4
 set swd = /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model
 
-# /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/ac/FlexFactorialBaleen/swra_slice
+/autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/ac/FlexFactorialBaleen/swra_slice
 
 
 ## Writes the m file that calls the surfrend_canonical script that creates the w files
-python write_surfrend_script.py $conname $connum $swd $pval $exthresh 
-  
-echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
- 
-## Runs the matlab script that creates the w files
-cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
-nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model/$conname'.m'
-
-## Writes the tickle script that specifies the tksurfer Snapshot images and locations
-python /cluster/kuperberg/SemPrMM/MRI/scripts/write_tickle_script.py $conname $swd lh $pval $exthresh ##Specifiy the hemisphere here, default lh
-python /cluster/kuperberg/SemPrMM/MRI/scripts/write_tickle_script.py $conname $swd rh $pval $exthresh
+# python write_surfrend_script.py $conname $connum $swd $pval $exthresh 
+#   
+# echo 'Calling' {$swd}/{$conname}'.m that runs the surfrend_canonical command' 
+#  
+# ## Runs the matlab script that creates the w files
+# cd /autofs/cluster/kuperberg/Software/spm/toolbox/surfrend/
+# nohup matlab7.11 -nosplash -nodesktop -nodisplay < /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model/$conname'.m'
+# 
+# # ## Writes the tickle script that specifies the tksurfer Snapshot images and locations
+# python /cluster/kuperberg/SemPrMM/MRI/scripts/write_tickle_script.py $conname $swd lh $pval $exthresh ##Specifiy the hemisphere here, default lh
+# python /cluster/kuperberg/SemPrMM/MRI/scripts/write_tickle_script.py $conname $swd rh $pval $exthresh
 
 ##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer 
 cd /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model
 tksurfer spm_canonical lh inflated -overlay $conname-lh.w -tcl $conname-lh.tcl
-#tksurfer fsaverage lh inflated -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
+#tksurfer fsaverage lh inflated -invphaseflag 1 -overlay $conname-lh.w -tcl /autofs/cluster/kuperberg/SemPrMM/MRI/scripts/wfile_to_rgb.tcl
+##-colorscalebarflag 0 -colscalebarvertflag 0
+##-scalebarflag 0 -invphaseflag 1
 
 ##convert the rgb images to jpeg
 cd /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model
@@ -47,7 +49,7 @@ convert $conname-lh-$pval-$exthresh-Med.rgb -colorspace RGB $conname-lh-$pval-$e
 ##Visualisation of SPM output(w files)in the inflated subject averages using tksurfer 
 cd /autofs/cluster/kuperberg/SemPrMM/MRI/results/SecondLevelStats/$model
 tksurfer spm_canonical rh inflated -overlay $conname-rh.w -tcl $conname-rh.tcl
-
+ 
 ##convert the rgb images to jpeg
 convert $conname-rh-$pval-$exthresh-Lat.rgb -colorspace RGB $conname-rh-$pval-$exthresh-Lat.jpg
 convert $conname-rh-$pval-$exthresh-Ven.rgb -colorspace RGB $conname-rh-$pval-$exthresh-Ven.jpg
