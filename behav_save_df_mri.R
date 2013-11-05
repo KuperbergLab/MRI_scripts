8b4fea4f2e9eeaaf36e8d4f52747bf78455e60a0
Candida Ustine <candida@nmr.mgh.harvard.edu>
2013-11-05 09:43:14 -0500

    renamed output file to fit the rest of the new accuracy scripts

--- behav_save_df_mri.R
+++ behav_save_df_mri.R
@@ -1,11 +1,11 @@
 behav_save_df_mri <-function(listPrefix,exp){
 	
 filePath <- "/cluster/kuperberg/SemPrMM/MRI/results/behavioral_accuracy/R/"
-fileName <- paste(filePath,'MRI_',listPrefix,'_',exp,"_accuracy.log",sep="")
+fileName <- paste(filePath,'MRI_',listPrefix,'_',exp,"_accuracy_long_new.log",sep="")
 buffer<-read.table(skip=1, fileName, header=FALSE)
 behavData.all<-data.frame(subj=factor(buffer$V1),cond=factor(buffer$V2),acc=buffer$V3)
 
-outFile <-paste(filePath, 'MRI_', listPrefix, '_', exp, '_accuracy.df', sep="")
+outFile <-paste(filePath, 'MRI_', listPrefix, '_', exp, '_accuracy_long_new.df', sep="")
 save(behavData.all, file=outFile)
 
 }
